a09bb61415f32246ec4e211a44570be9acf2f749
Jamila Toaha <jtoahacoding@gmail.com>
2020-03-05 23:57:26 -0500

    updated form lab css + added undo functionality

--- CISC 3610 Labs/form lab/index.js
+++ CISC 3610 Labs/form lab/index.js
@@ -1,43 +1,90 @@
 let pageTitle = localStorage.getItem('title');
-
 let pageNote = localStorage.getItem('note');
 
-if(pageTitle){
-    document.getElementById("title").value = pageTitle;
+let totalUndos = 0;
+
+let undoArray = [];
+
+if(localStorage.getItem('undo1')) undoArray.push(localStorage.getItem('undo1'))
+if(localStorage.getItem('undo2')) undoArray.push(localStorage.getItem('undo2'))
+if(localStorage.getItem('undo3')) undoArray.push(localStorage.getItem('undo3'))
+
+//If these values for title and note already exist in local storage, display them
+if (pageTitle){
+    document.getElementById('title').value = pageTitle;
 }
 
-if(pageNote) {
-  document.getElementById("note").value = pageNote;
+if (pageNote) {
+  document.getElementById('note').value = pageNote;
 }
 
-// document.getElementById("title");
 
+function setTitle(){
+  let title = document.getElementById('title').value;
+  localStorage.setItem('title', title);
 
-function setTitle() {
-  let title = document.getElementById("title").value;
+  //if the current character is a space, create a back-up
+  if (title[title.length - 1] === ' ') {
+    totalUndos++;
 
-  localStorage.setItem("title", title);
-  console.log(title)
+    undoArray.push('type: ' + title.slice(0, title.length - 1));
+  }
+
 }
 
 function setNote(){
-  let note = document.getElementById("note").value;
+  let note = document.getElementById('note').value;
+  localStorage.setItem('note', note);
 
-  localStorage.setItem("note", note);
-  console.log(note)
+  if (note[note.length - 1] === ' ') {
+    totalUndos++;
+    undoArray.push('note: ' + note.slice(0, note.length - 1));
+
+    if(undoArray.length == 1) {
+      localStorage.setItem( 'undo1', undoArray[0]);
+    }
+    if(undoArray.length == 2) {
+      localStorage.setItem( 'undo1', undoArray[0]);
+      localStorage.setItem( 'undo2', undoArray[1]);
+    }
+    if(undoArray.length >= 3) {
+      localStorage.setItem( 'undo1', undoArray[undoArray.length-3]);
+      localStorage.setItem( 'undo2', undoArray[undoArray.length-2]);
+      localStorage.setItem( 'undo3', undoArray[undoArray.length-1]);
+    }
+
+  }
+
 }
 
 
 function clearFields(){
-  localStorage.setItem("title", "")
-  localStorage.setItem("note", "");
-  document.getElementById("title").value = localStorage.getItem("title");
-  document.getElementById("note").value = localStorage.getItem("title");
+  localStorage.setItem('title', '')
+  localStorage.setItem('note', '');
+  totalUndos = 0;
+  undoArray = [];
+  document.getElementById('title').value = localStorage.getItem('title');
+  document.getElementById('note').value = localStorage.getItem('title');
 }
 
-/*still need to work on this*/
 function undo(){
-  console.log("testing");
+
+  //If array is empty, don't do anything
+  if(undoArray.size < 1) return;
+
+  let currentUndo = undoArray.pop(); //this is an object
+
+
+    if(currentUndo.startsWith('title')) {
+    document.getElementById('title').value = currentUndo.slice(6);
+    document.getElementById('title').innerText = currentUndo.slice(6);
+    localStorage.setItem('title', currentUndo.slice(6));
+  }
+
+  if(currentUndo.startsWith('note')) {
+    document.getElementById('note').value = currentUndo.slice(5);
+    document.getElementById('note').innerText = currentUndo.slice(5);
+    localStorage.setItem('note', currentUndo.slice(5));
+  }
+
 }
-
-
