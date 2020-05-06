1d03e33b4d7c65631047116a18a5737369d9904c
Jamila Toaha <jtoahacoding@gmail.com>
2020-05-06 00:58:39 -0400

    added Data Visualization bar chart

--- CISC 3610 Projects/DataVisualizationProject/main.js
+++ CISC 3610 Projects/DataVisualizationProject/main.js
@@ -1,25 +1,61 @@
+/**
+ * Jamila Toaha
+ * 05/06/20
+ * Data Visualization Project
+ * As required by assignment, data for charts are loaded from an external file via an ajax request.
+ */
+function start(data){
+  var ctx = document.getElementById('bar-chart').getContext('2d');
 
-function start(){
-  var ctx = document.getElementById('myChart').getContext('2d');
+  var [years, millionGallonsPerDay] = sortData(data);
 
-  var chart = new Chart(ctx, {
-      // The type of chart we want to create
-      type: 'line',
+  // Bar chart
+    var barChart = new Chart(document.getElementById('bar-chart'), {
+    type: 'horizontalBar',
+    data: {
+      labels: [...years],
+      datasets: [
+        {
+          label: 'NYC Consumption (Million gallons per day)',
+          backgroundColor: '#3e95cd',
+          data: [...millionGallonsPerDay]
+        }
+      ]
+    },
+    options: {
+      legend: { display: true },
+      title: {
+        display: true,
+        text: 'A History of Water Consumption In New York City'
+      }
+    }
+});
 
-      // The data for our dataset
-      data: {
-          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-          datasets: [{
-              label: 'My First dataset',
-              backgroundColor: 'rgb(255, 99, 132)',
-              borderColor: 'rgb(255, 99, 132)',
-              data: [0, 10, 5, 2, 20, 30, 45]
-          }]
-      },
-
-      // Configuration options go here
-      options: {}
-  });
 }
 
-document.addEventListener('DOMContentLoaded', start);
+// document.addEventListener('DOMContentLoaded', start);
+
+
+//As required by assignment, data for charts are loaded from an external file via an ajax request.
+$.ajax({
+    url: "https://data.cityofnewyork.us/resource/ia2d-e54m.json",
+    type: "GET",
+    data: {
+      "$limit" : 5000,
+      "$$app_token" : "Ai3RBUGUNk1e3ntZxES7UA9bd"
+    }
+}).done(function(data) {
+  alert("Retrieved " + data.length + " records from the dataset!");
+  console.log(data);
+    start(data);
+});
+
+function sortData(data){
+    var years = [];
+    var millionGallonsPerDay = [];
+    for(let datum of data){
+        years.push(datum.year);
+        millionGallonsPerDay.push(datum.nyc_consumption_million_gallons_per_day)
+    }
+    return [years, millionGallonsPerDay]
+}
